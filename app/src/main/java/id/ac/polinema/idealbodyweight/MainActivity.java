33b98d215d91855639fb24a3c859697d3074a19f
firmancitranurrahman <firmancitranurrahman@gmail.com>
2019-09-30 14:43:08 +0700

    Menyelesaikan Praktikum AboutFragment (bundle)

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,26 +7,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import id.ac.polinema.idealbodyweight.fragment.AboutFragment;
 
+public class MainActivity extends AppCompatActivity {
+	private AboutFragment aboutFragment;
 	// Deklarasikan atribut Fragment di sini
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		aboutFragment = AboutFragment.newInstance("Firman Citra Nurrahman");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.menu, menu);
 		return  true;
+
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 		// TODO: Tambahkan penanganan menu di sini
-
+		if (item.getItemId()==R.id.menu_about){
+			getSupportFragmentManager().beginTransaction()
+					.replace(R.id.fragment_container,aboutFragment)
+					.addToBackStack(null)
+					.commit();
+		}
 		return super.onOptionsItemSelected(item);
 	}
 }
